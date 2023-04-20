ece2c42c861ff2a7d0e698d62ab82f1a6b1880d0
francesc fores <ffores93@gmail.com>
2023-04-20 12:40:47 +0200

    challenge 1

--- challenge_1/challenge.js
+++ challenge_1/challenge.js
@@ -1,15 +1,77 @@
+He explicado este reto en la seccion Análisis corregirias algo?
+
 function wrapping(gifts) {
     let $aux=[];
     gifts.forEach((gift, index, self) => {
         $aux[index]=['*']
-        let prepared=true;
-        while(prepared) {
+        let prepared=false;
+        while(!prepared) {
             $aux[index]='*'+$aux[index];
-            if(gift.length+2<=$aux[index].length){
-                prepared = false;
-                $aux[index]=$aux[index]+'\n*'+gift+'*\n'+$aux[index];
+            if(gift.length+2===$aux[index].length){
+                console.log($aux[index])
+                let up = $aux[index]
+                let center = '\n*'+gift+'*\n';
+                let down = $aux[index];
+                prepared = true;
+
             }
         }
     })
     return $aux;
 }
+
+
+const gifts = ['cat', 'game', 'socks']
+// const gifts = ['a']
+// const gifts = []
+const wrapped = wrapping(gifts)
+
+console.log(wrapped)
+
+## Análisis
+La función wrapping toma un array de regalos (gifts) como entrada y devuelve un nuevo array que
+contiene los envoltorios de los regalos hechos con asteriscos.
+
+    Paso 1: Se crea un array ($aux) que contendrá los regalos envueltos.
+
+    Paso 2: Se recorren los regalos en el array gifts utilizando un forEach.
+
+    Paso 3: En cada iteración, se prepara el regalo agregando un asterisco (*)
+como base en el array de envoltorios ($aux[index]).
+
+    Paso 4: Se utiliza una variable booleana (prepared) para indicar si el regalo está completamente envuelto o no.
+    Se inicia con el valor false.
+
+    Paso 5: Se crea un bucle while que se ejecuta hasta que el regalo esté completamente envuelto (prepared sea false).
+
+Paso 6: Dentro del bucle, se sigue envolviendo el regalo agregando asteriscos al array de envoltorios ($aux[index]).
+    Luego se compara si la longitud del regalo más 2 es igual a la longitud del envoltorio.
+    Si es así, significa que el regalo está preparado para envolver, por ejemplo:
+
+    Regalo: "cat"
+Longitud de "cat": 3
+Envoltorio: "*****" ($aux[index])
+"*****"
+"*cat*"  // los asteriscos en los bordes se agregan en el siguiente paso, esta representación es solo para entender el caso
+
+Si no se suma 2 a la longitud del regalo, se vería algo como esto:
+
+    Regalo: "cat"
+Longitud de "cat": 3
+"***"
+"*cat*"
+
+Este paso asegura que el regalo esté completamente envuelto con asteriscos en cada lado,
+    es decir, haya al menos dos asteriscos en cada extremo del regalo.
+
+    Paso 7: Se crea una variable up que contiene el envoltorio de la parte superior del regalo ($aux[index]).
+
+    Paso 8: Se crea una variable center que contiene el regalo envuelto con un salto de línea y asteriscos antes y después del regalo: "\n*"+gift+"*\n".
+
+    Paso 9: Se crea una variable down que contiene el envoltorio de la parte inferior del regalo, que es igual al envoltorio de la parte superior ($aux[index]).
+
+    Paso 10: Se unen todas las partes del regalo (up, center, down) para obtener el regalo completamente envuelto.
+
+    Paso 11: Una vez que el regalo está completamente envuelto, se asigna true a la variable prepared para salir del bucle while.
+
+Paso 12: Se repite el proceso para el siguiente regalo en caso de que exista en el array gifts.
